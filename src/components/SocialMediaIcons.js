c8c8ab4616f07ee7beb57d057e2ff460a3060db5
malhotra-parul <parulm.business@gmail.com>
2020-04-22 13:18:21 +0530

    completed Header module with media queries support

--- src/components/SocialMediaIcons.js
+++ src/components/SocialMediaIcons.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { UnorderedList,  Link, IconWrapper } from "../styles";
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebookF, faTwitter, faGithub, faLinkedinIn, faInstagram } from '@fortawesome/free-brands-svg-icons';
@@ -7,13 +8,13 @@ library.add(faFacebookF, faTwitter, faGithub, faLinkedinIn, faInstagram);
 const SocialMedia = () => {
     return (
         <div>
-            <ul>
-            <li><FontAwesomeIcon icon={faFacebookF} color="green" /></li>
-            <li><FontAwesomeIcon icon={faTwitter} color="green"/></li>
-            <li><FontAwesomeIcon icon={faGithub} color="green"/></li>
-            <li><FontAwesomeIcon icon={faLinkedinIn} color="green"/></li>
-            <li><FontAwesomeIcon icon={faInstagram} color="green"/></li>
-            </ul>
+            <UnorderedList>
+            <li><Link href=""><IconWrapper><FontAwesomeIcon icon={faFacebookF} color="#549cdf" size="xs" /></IconWrapper></Link></li>
+            <li><Link href=""><IconWrapper><FontAwesomeIcon icon={faTwitter} color="#00aced" size="xs"/></IconWrapper></Link></li>
+            <li><Link href=""><IconWrapper><FontAwesomeIcon icon={faGithub} color="#dd4b39" size="xs"/></IconWrapper></Link></li>
+            <li><Link href=""><IconWrapper><FontAwesomeIcon icon={faLinkedinIn} color="#007bb6" size="xs"/></IconWrapper></Link></li>
+            <li><Link href=""><IconWrapper><FontAwesomeIcon icon={faInstagram} color="#e4405f" size="xs"/></IconWrapper></Link></li>
+            </UnorderedList>
         </div>
       );
 }
--- src/styles.js
+++ src/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 
 export const Container = styled.div`
-    margin: 20px;
+    margin: 0;
 `
 
 export const Wrapper = styled.div`
@@ -45,16 +45,82 @@ export const Sample = styled.div`
 
 export const Image = styled.img`
   width: 10em;
-  height: 2em;
   position: relative;
+  top:20px;
   left: 20px;
+  @media (max-width: 550px){
+    left:0;
+    text-align: center;
+    padding: 10px;
+  }
 `;
 
 export const Heading = styled.header`
-  margin-bottom:20px;
+  margin-bottom: 20px;
   padding:0;
   width: 100%;
   height: auto;
   display: flex;
   justify-content: space-between;
+  align-items: center;
+
+  @media (max-width: 550px){
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
 `;
+
+export const UnorderedList = styled.ul`
+  position: relative;
+  right: 20px;
+  top:20px;
+  display:flex;
+  margin: 0;
+  padding: 0;
+  justify-content: space-evenly;
+  li{
+    list-style: none;
+  }
+  @media (max-width: 550px){
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    right: 0;
+    padding: 10px;
+  }
+
+`;
+
+export const Link = styled.a`
+  position: relative;
+  width:40px;
+  height:40px;
+  display:block;
+  text-align:center;
+  margin: 0 10px;
+  border-radius: 50%;
+  padding: 6px;
+  box-sizing: border-box;
+  text-decoration:none;
+  box-shadow: 0 10px 15px rgba(0,0,0,0.3);
+  background: linear-gradient(0deg, #ddd, #fff );
+  transition: .5s;
+  & :hover{
+    box-shadow: 0 2px 5px rgba(0,0,0,0.3);
+  }
+`;
+
+export const IconWrapper = styled.div`
+  display: block;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(0deg, #fff, #ddd);
+  border-radius: 50%;
+  line-height: calc( - 12px);
+  font-size:24px;
+  color: #262626;
+  transition: .09s;
+`;
+
